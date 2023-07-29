5a557145cbcbb6a7505118a2b83976e822f60aa8
Zelaux <ura20060605@gmail.com>
2023-07-29 20:09:15 +0300

    Replaced Consumer -> Cons

--- util/src/me13/core/queue/Queue.java
+++ util/src/me13/core/queue/Queue.java
@@ -1,8 +1,8 @@
 package me13.core.queue;
 
+import arc.func.Cons;
 import arc.util.Log;
 import org.jetbrains.annotations.Contract;
-import java.util.function.Consumer;
 
 public class Queue<T> {
     public final T value;
@@ -12,15 +12,15 @@ public class Queue<T> {
         this.value = value;
     }
 
-    public void process(Consumer<T> handler) {
+    public void process(Cons<T> handler) {
         process(handler, Log::err);
     }
 
-    public void process(Consumer<T> handler, Consumer<Throwable> fail) {
+    public void process(Cons<T> handler, Cons<Throwable> fail) {
         try {
-            handler.accept(value);
+            handler.get(value);
         } catch(Throwable throwable) {
-            fail.accept(throwable);
+            fail.get(throwable);
         }
     }
 }
